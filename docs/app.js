0b84873a3106ef006f45221be6c541c21d0578cc
Scizors <scizors@pop-os.localdomain>
2021-01-20 02:20:54 -0800

    typo fix

--- docs/app.js
+++ docs/app.js
@@ -3656,7 +3656,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var reac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"BioBlock\": () => /* binding */ BioBlock\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nconst BioBlock = props => {\n  console.log(props);\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \" Under Contsruction \"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"p\", null));\n};\n\n//# sourceURL=webpack://personal-web-page/./src/BioBlock.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"BioBlock\": () => /* binding */ BioBlock\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nconst BioBlock = props => {\n  console.log(props);\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \" Under Construction \"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"p\", null));\n};\n\n//# sourceURL=webpack://personal-web-page/./src/BioBlock.jsx?");
 
 /***/ }),
 
--- src/BioBlock.jsx
+++ src/BioBlock.jsx
@@ -3,7 +3,7 @@ export const BioBlock = (props) => {
     console.log(props);
     return (
         <div> 
-            <h1> Under Contsruction </h1>
+            <h1> Under Construction </h1>
             <p> 
                         
             </p>
