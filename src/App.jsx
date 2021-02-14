0caa1607d44a640b9271505be51f095addff691b
Scizors <scizors@pop-os.localdomain>
2021-02-14 11:56:28 -0800

    working on pushing parcel version to web

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
 
-
 import Content from "./Content";
 import NavBar from "./NavBar";
 import GlobalStyled from "./GlobalStyled";
-import Favicon from "./assets/favicon-32x32.png";
-
 
 const App = (
     <Router>
